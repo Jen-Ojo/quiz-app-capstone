1238e7d0657c5a0bcf4125961d3bc4a5d52804f7
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-03-08 12:48:04 +0100

    fix App.jsx, main.jsx, and Vite config for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx"; // must match file name exactly
+
+createRoot(document.getElementById("root")).render(<App />);
